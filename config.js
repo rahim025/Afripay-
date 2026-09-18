580621c704add6f36b37d58f2501c3bdc5f97626
rahim025 <rahimbatchabi7@gmail.com>
2026-09-18 20:55:04 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // En local (serveur + navigateur sur le même ordinateur) : http://localhost:5000
 // Backend déployé sur Render :
 
-window.AFRIPAY_API_BASE = 'https://afripay0.onrender.com';
+window.AFRIPAY_API_BASE = 'https://afripay-9uw9.onrender.com';
